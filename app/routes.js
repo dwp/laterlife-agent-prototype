79508b27397ba81d3a8e91041b6933b4acd0f752
Ross.B <81567432+RossDavidBrown@users.noreply.github.com>
2024-05-10 12:07:18 +0100

    Initial commit
    GitOrigin-RevId: 284ae6c249465063af2c44ff53b0a45a58e1423e

--- app/routes.js
+++ app/routes.js
@@ -6,19 +6,4 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-
-router.use('/retirement-showcase', require('./views/retirement-showcase/routes'))
-router.use('/7-0', require('./views/7-0/routes'))
-router.use('/6-0', require('./views/6-0/routes'))
-router.use('/5-0', require('./views/5-0/routes'))
-router.use('/4-0', require('./views/4-0/routes'))
-router.use('/3-0', require('./views/3-0/routes'))
-router.use('/2-0', require('./views/2-0/routes'))
-router.use('/1-0', require('./views/1-0/routes'))
-
-
-
-
-
-
-module.exports = router
+// Add your routes here
