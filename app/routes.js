7cbf99370ad546c3ecdf84245f7b92c8dea0eac6
Rachel Robson <rachel.robson@engineering.digital.dwp.gov.uk>
2024-06-07 21:09:52 +0100

    linking up bank details, tidying up navigation options
    GitOrigin-RevId: 5dcfff41de565878b896c437c7f76d1436fe4f0b

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,48 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// MVP VERSION 0
+
+// Run this code when a form is submitted to 'location-of-account'
+router.post('/location-of-account', function (req, res) {
+
+    // Make a variable and give it the value from 'account-location'
+    var accountLocation = req.session.data['account-location']
+  
+    // Check whether the variable matches a condition
+    if (accountLocation == "uk"){
+      // Send user to next page
+      res.redirect('/mvp/0/account-change/account-details')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/mvp/0/account-change/overseas')
+    }
+  
+  });
+
+
+  // Run this code when a form is submitted to 'confirm-bank-name'
+router.post('/confirm-bank-name', function (req, res) {
+
+    // Make a variable and give it the value from 'confirm-bank'
+    var bankNameConfirm = req.session.data['confirm-bank']
+  
+    // Check whether the variable matches a condition
+    if (bankNameConfirm == "yes"){
+      // Send user to next page
+      res.redirect('/mvp/0/account-change/check-answers')
+    } else {
+      // Send user bank to enter account details page
+      res.redirect('/mvp/0/account-change/account-details')
+    }
+  
+  });
+
+//-------------------------------------------------------------------
+
+
+// MVP VERSION 1
+
+
+//-------------------------------------------------------------------
