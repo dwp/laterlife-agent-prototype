9f4055ed856e3533ae0d016544d09c818db3e2e9
Rachel Robson <rachel.robson@engineering.digital.dwp.gov.uk>
2024-06-11 14:35:36 +0100

    fixing return journey from end of change bank

--- app/routes.js
+++ app/routes.js
@@ -28,7 +28,25 @@ router.post('/location-of-account', function (req, res) {
   });
 
 
-  // Run this code when a form is submitted to 'confirm-bank-name'
+// Run this code when a form is submitted to 'location-of-account' in option 1 nav
+router.post('/location-of-account0', function (req, res) {
+
+  // Make a variable and give it the value from 'account-location'
+  var accountLocation0 = req.session.data['account-location']
+
+  // Check whether the variable matches a condition
+  if (accountLocation0 == "uk"){
+    // Send user to next page
+    res.redirect('/mvp/0/nav-options/option-1-full/account-change/account-details')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/mvp/0/nav-options/option-1-full/account-change/overseas')
+  }
+
+});
+
+
+// Run this code when a form is submitted to 'confirm-bank-name'
 router.post('/confirm-bank-name', function (req, res) {
 
     // Make a variable and give it the value from 'confirm-bank'
@@ -44,6 +62,24 @@ router.post('/confirm-bank-name', function (req, res) {
     }
   
   });
+
+
+// Run this code when a form is submitted to 'confirm-bank-name' in option 1 nav
+router.post('/confirm-bank-name0', function (req, res) {
+
+  // Make a variable and give it the value from 'confirm-bank'
+  var bankNameConfirm0 = req.session.data['confirm-bank']
+
+  // Check whether the variable matches a condition
+  if (bankNameConfirm0 == "yes"){
+    // Send user to next page
+    res.redirect('/mvp/0/nav-options/option-1-full/account-change/check-answers')
+  } else {
+    // Send user bank to enter account details page
+    res.redirect('/mvp/0/nav-options/option-1-full/account-change/account-details')
+  }
+
+});
 
 //-------------------------------------------------------------------
 
