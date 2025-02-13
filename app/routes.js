1b8944b00bcbb4d2bac5a30c711e625afdd0b1a5
westonjen123 <48479197+westonjen123@users.noreply.github.com>
2025-02-13 10:02:57 +0000

    Updates to prev version routes

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,9 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+router.use('/1-0', require('./views/1-0/routes'))
+router.use('/2-0', require('./views/2-0/routes'))
+router.use('/3-0', require('./views/3-0/routes'))
 router.use('/4-0', require('./views/4-0/routes'))
 router.use('/5-0', require('./views/5-0/routes'))
 
