c1ec71347bfb495232fdc049d5f992e38a14d10b
westonjen123 <48479197+westonjen123@users.noreply.github.com>
2025-02-20 09:47:46 +0000

    Updated routing
    All version fixed

--- app/views/2-0/routes.js
+++ app/views/2-0/routes.js
@@ -8,9 +8,58 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-// MVP VERSION 2
+// MVP VERSION 5
+
+// Move to CAM or GySP journey
 
 
+// Run this code when you are sure the change of circ is a date in the future
+router.post('/confirm-future', function (req, res) {
+
+  // Make a variable and give it the value from 'confirm-bank'
+  var confirmFuture = req.session.data['confirm-future']
+
+  // Check whether the variable matches a condition
+  if (confirmFuture == "yes"){
+    // Send user to confirm page
+    res.redirect('remove-claimant-date-confirm-future')
+  } else {
+    // Send user back to other confirm page 
+    res.redirect('remove-claimant-date')
+  }
+
+});
+
+
+// Run this code when you want to choose to pay the next PC payment in the remove claimant journey
+router.post('/payNextConfirm', function (req, res) {
+
+  // Make a variable and give it the value from 'confirm-bank'
+  var payNextConfirm = req.session.data['payNextConfirm']
+
+  // Check whether the variable matches a condition
+  if (payNextConfirm == "yes"){
+    // Send user to confirm page
+    res.redirect('remove-claimant-pay-next-confirm-A')
+  } else {
+    // Send user back to other confirm page 
+    res.redirect('remove-claimant-pay-next-confirm-B')
+  }
+
+});
+
+
+// Radio buttons page 'Why do you want to move the claimant?' 
+
+router.post('/selectReason', function(request, response) {
+
+  var RemovePC = request.session.data['removeReason']
+  if (RemovePC == "entitled"){
+      response.redirect('remove-claimant-pay-next')
+  } else {
+      response.redirect('remove-claimant-date')
+  }
+})
 
 
 // Change contact details 
@@ -25,10 +74,10 @@ router.post('/contact-remove-email', function (req, res) {
   // Check whether the variable matches a condition
   if (ContactRemove == "yes"){
     // Send user to next page
-    res.redirect('/2-0//contact-success-remove-email')
+    res.redirect('contact-success-remove-email')
   } else {
     // Send user to name incorrect page 
-    res.redirect('/2-0//contact')
+    res.redirect('contact')
   }
 
 });
@@ -44,10 +93,10 @@ router.post('/contact-remove-homephone', function (req, res) {
   // Check whether the variable matches a condition
   if (ContactRemove == "yes"){
     // Send user to next page
-    res.redirect('/2-0/contact-success-remove-homephone.html')
+    res.redirect('contact-success-remove-homephone.html')
   } else {
     // Send user to name incorrect page 
-    res.redirect('/2-0/contact')
+    res.redirect('contact')
   }
 
 });
@@ -61,10 +110,10 @@ router.post('/contact-remove-mobile', function (req, res) {
   // Check whether the variable matches a condition
   if (ContactRemove == "yes"){
     // Send user to next page
-    res.redirect('/2-0/contact-success-remove-mobile')
+    res.redirect('contact-success-remove-mobile')
   } else {
     // Send user to name incorrect page 
-    res.redirect('/2-0/contact')
+    res.redirect('contact')
   }
 
 });
@@ -75,36 +124,20 @@ router.post('/select-address', function(request, response) {
 
   var selectAddress = request.session.data['select-address']
   if (selectAddress == "1 Elm Street"){
-      response.redirect("/2-0/contact-success-change-address")} 
+      response.redirect('contact-success-change-address')} 
   else if (selectAddress == "10 Elm Street"){
-        response.redirect("/2-0/contact-success-change-address")} 
+        response.redirect('contact-success-change-address')} 
   else if (selectAddress == "11 Elm Street"){
-          response.redirect("/2-0//contact-success-change-address")} 
+          response.redirect('contact-success-change-address')} 
   else {
-    response.redirect("/2-0/change-address-manual")
+    response.redirect('change-address-manual')
 } 
   
 })
 
 
 
-// Run this code when a form is submitted to 'bank-name-answer'
-router.post('/changed-bank-answer', function (req, res) {
-
-  // Make a variable and give it the value from 'confirm-bank'
-  var bankNameConfirm = req.session.data['changed-bank']
-
-  // Check whether the variable matches a condition
-  if (bankNameConfirm == "yes"){
-    // Send user to next page
-    res.redirect('/2-0/bank-cya')
-  } else {
-    // Send user to name incorrect page 
-    res.redirect('/2-0/bank-name-incorrect')
-  }
-
-});
-
+// MVP VERSION 3
 
 
 // Suspend journey code (no longer used in v4 or above) 
@@ -118,15 +151,13 @@ router.post('/confirm-sp-suspend', function (req, res) {
   // Check whether the variable matches a condition
   if (suspendSPConfirm == "yes"){
     // Send user to next page
-    res.redirect('/2-0/personal-suspended-sp-both')
+    res.redirect('personal-suspended-sp-both')
   } else {
     // Send user back to the personal tab
-    res.redirect('/2-0/personal-suspended-no-success')
+    res.redirect('personal-suspended-no-success')
   }
 
 });
-
-
 
 // Run this code when when SP is already suspended and a user wants to also suspend PC. Radios are on the are you sure page. 
 router.post('/confirm-pc-suspend', function (req, res) {
@@ -137,24 +168,32 @@ router.post('/confirm-pc-suspend', function (req, res) {
   // Check whether the variable matches a condition
   if (suspendPCConfirm == "yes"){
     // Send user to next page
-    res.redirect('/2-0/personal-suspended-pc-both')
+    res.redirect('personal-suspended-pc-both')
   } else {
     // Send user back to the personal tab
-    res.redirect('/2-0/personal-suspended-no-success')
+    res.redirect('personal-suspended-no-success')
   }
 
 });
 
 
 
+
+// MVP VERSION 2
+
+
+
+
+
+
 // Run this code when a wants to suspend a benefit - Death or both 
 router.post('/stop-benefit', function (req, res) {
   if  (req.body['stopbenefit'] === 'suspend') {
-    res.redirect('/2-0/suspend-benefit')
+    res.redirect('suspend-benefit')
   } else if (req.body['stopbenefit'] === 'death') {
-    res.redirect('/2-0/personal-suspended-death')
+    res.redirect('personal-suspended-death')
   } else {
-    res.redirect('/2-0/stop-benefit')
+    res.redirect('stop-benefit')
   }
 })
 
@@ -164,11 +203,11 @@ router.post('/stop-benefit', function (req, res) {
 // Run this code when a user has stopped both SP & PC and then they want to start a payment again
 router.post('/restart', function (req, res) {
   if (req.body['restart'] === 'both') {
-    res.redirect('/2-0/personal-restarted')
+    res.redirect('personal-restarted')
   } else if (req.body['restart'] === 'sp') {
-    res.redirect('/2-0/personal-restarted-sp-only')
+    res.redirect('personal-restarted-sp-only')
   } else if (req.body['restart'] === 'pc') {
-    res.redirect('/2-0/personal-restarted-pc-only')
+    res.redirect('personal-restarted-pc-only')
   }
 })
 
@@ -181,10 +220,10 @@ router.post('/changed-restart-answer', function (req, res) {
   // Check whether the variable matches a condition
   if (restartPayments == "yes"){
     // Send user to restart page
-    res.redirect('/2-0/personal-restarted-pc')
+    res.redirect('personal-restarted-pc')
   } else {
     // Send user to suspended page
-    res.redirect('/2-0/personal-suspended-no-success')
+    res.redirect('personal-suspended-no-success')
   }
 
 });
@@ -199,10 +238,10 @@ router.post('/changed-restart-answer-sp', function (req, res) {
   // Check whether the variable matches a condition
   if (restartPaymentsSP == "yes"){
     // Send user to restart page
-    res.redirect('/2-0/personal-restarted-sp')
+    res.redirect('personal-restarted-sp')
   } else {
     // Send user to suspended page
-    res.redirect('/2-0/personal-suspended-no-success')
+    res.redirect('personal-suspended-no-success')
   }
 
 });
@@ -216,10 +255,10 @@ router.post('/confirm-death', function (req, res) {
   // Check whether the variable matches a condition
   if (confirmDeath == "yes"){
     // Send user to restart page
-    res.redirect('/2-0/personal-suspended-death')
+    res.redirect('personal-suspended-death')
   } else {
     // Send user to suspended page
-    res.redirect('/2-0/personal-suspended-no-success')
+    res.redirect('personal-suspended-no-success')
   }
 
 });
@@ -227,6 +266,10 @@ router.post('/confirm-death', function (req, res) {
 
 
 // Run this code when a user wants to choose to suspend due to death or SP only
+
+
+
+
 router.post('/stop-benefit-death-sp', function (req, res) {
 
   // Make a variable and give it the value from 'death confirm'
@@ -235,10 +278,10 @@ router.post('/stop-benefit-death-sp', function (req, res) {
   // Check whether the variable matches a condition
   if (confirmDeathSP == "death"){
     // Send user to restart page
-    res.redirect('/2-0/personal-suspended-death')
+    res.redirect('personal-suspended-death')
   } else {
     // Send user to suspended page
-    res.redirect('/2-0/personal-suspended-sp-both')
+    res.redirect('personal-suspended-sp-both')
   }
 
 });
@@ -253,13 +296,32 @@ router.post('/stop-benefit-death-pc', function (req, res) {
   // Check whether the variable matches a condition
   if (confirmDeathPC == "death"){
     // Send user to restart page
-    res.redirect('/2-0/personal-suspended-death')
+    res.redirect('personal-suspended-death')
   } else {
     // Send user to suspended page
-    res.redirect('/2-0/personal-suspended-pc-both')
+    res.redirect('personal-suspended-pc-both')
   }
 
 });
+
+// Change bank name answer
+
+router.post('/changed-bank-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'death confirm'
+  var changeBankName = req.session.data['changed-bank']
+
+  // Check whether the variable matches a condition
+  if (changeBankName == "yes"){
+    // Send user to restart page
+    res.redirect('payment-success')
+  } else {
+    // Send user to suspended page
+    res.redirect('bank-name-incorrect')
+  }
+
+});
+
 
 
 
