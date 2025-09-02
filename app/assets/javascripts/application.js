522499ad0f3eb750e4dd2ec8e702d43071b033c5
JonathanInch <jonathan.inch1@engineering.digital.dwp.gov.uk>
2025-09-02 14:45:34 +0100

    Reworked marital details page to match v5
    GitOrigin-RevId: 38141234aafe822a2858eac96daeaa400648fbea

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,4 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
-
-
-
-
 })
--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,6 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 router.use('/retirement-showcase', require('./views/retirement-showcase/routes'))
-router.use('/9-0', require('./views/9-0/routes'))
 router.use('/8-0', require('./views/8-0/routes'))
 router.use('/7-0', require('./views/7-0/routes'))
 router.use('/6-0', require('./views/6-0/routes'))
