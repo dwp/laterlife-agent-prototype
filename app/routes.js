2b194694eadbb94f67674cf9d5ce1e6362f7a0ed
JonathanInch <jonathan.inch1@engineering.digital.dwp.gov.uk>
2025-09-09 09:32:56 +0100

    Added CWP to payment and details
    GitOrigin-RevId: 3f6db521b695fbfe780a16c50b184d44fb061bf7

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 router.use('/retirement-showcase', require('./views/retirement-showcase/routes'))
+router.use('/10-0', require('./views/10-0/routes'))
 router.use('/9-0', require('./views/9-0/routes'))
 router.use('/8-0', require('./views/8-0/routes'))
 router.use('/7-0', require('./views/7-0/routes'))
